4d2c0ae6c86f14b71dad96f8926851d61609f092
Anne Ogborn <annie@thelginworks.com>
2017-07-10 11:24:22 -0700

    cleaned misparsed emails, made sample swinb

--- config-available/email_analysis.pl
+++ config-available/email_analysis.pl
@@ -1,5 +1,8 @@
 :- module(email_analysis, [load_enron/0,
-                          prefixed_string/2]).
+                          prefixed_string/2,
+                          bogus_predicate/1,
+                          load_enron_db/0,
+                          save_enron_db/0]).
 /** <module> Tools to analyze emails
  *
  *  If you use this code without pomny permission you agree to forever
@@ -77,6 +80,9 @@ sandbox:safe_primitive(rdf_portray:rdf_portray_as(_)).
 
 :- setting(email_analysis:enron_turtle, acyclic, 'enronrdf.ttl',
            'path or abstract path as prolog style path to email turtle file').
+:- setting(email_analysis:enron_db, acyclic, 'cleanenron.db',
+           'path or abstract path as prolog style path to email db file').
+
 
 :- rdf_register_prefix(enron, 'http://pomny.io/rdf/enron/').
 :- rdf_register_prefix(enronschema,  'http://pomny.io/rdf/enron/relation/').
@@ -84,7 +90,16 @@ sandbox:safe_primitive(rdf_portray:rdf_portray_as(_)).
 
 load_enron :-
     setting(email_analysis:enron_turtle, TTL),
-    rdf_load(TTL, []).
+    rdf_load(TTL, []),
+    retract_bogus.
+
+load_enron_db :-
+    setting(email_analysis:enron_db, DB),
+    rdf_load_db(DB).
+
+save_enron_db :-
+    setting(email_analysis:enron_db, DB),
+    rdf_save_db(DB).
 
 sandbox:safe_primitive(email_analysis:load_enron).
 
@@ -94,6 +109,18 @@ prefixed_string(IRI, S) :-
     with_output_to(string(S), print(IRI)).
 
 sandbox:safe_primitive(email_analysis:prefixed_string(_,_)).
+
+bogus_predicate(P) :-
+    rdf_predicate(P),
+    atom_concat('http://pomny.io/rdf/enron/relation/%20',
+                       _, P).
+retract_bogus :-
+    bogus_predicate(B),
+    rdf(IRI, B, _),
+    rdf(IRI, rdf:type, enronschema:email),
+    rdf_retractall(IRI, _, _),
+    fail.
+retract_bogus.
 
 :- endif.
 
